ff6ba40a52c065474193f4d674a856118d84b8e5
huanghaixin@126.com <huanghaixin@126.com>
2020-02-27 21:44:39 +0800

    #1 - Fixed #1, fixed #6, only allow wecube hosts access github.com and some tencent cloud api domain

--- delivery-wecube-for-production/scripts/wecube/install-squid.sh
+++ delivery-wecube-for-production/scripts/wecube/install-squid.sh
@@ -3,6 +3,26 @@
 echo "Starting squid ..."
 
 yum install squid -y
-#docker run --name squid -d --restart=always --publish 3128:3128 --volume /srv/docker/squid/cache:/var/spool/squid3 sameersbn/squid
+
+sed -i "1i\cache_mem 64 MB" /etc/squid/squid.conf
+sed -i "1i\http_access deny wecubehosts all" /etc/squid/squid.conf
+sed -i "1i\http_access allow wecubehosts whitelist" /etc/squid/squid.conf
+sed -i "1i\acl whitelist dstdomain -i cvm.tencentcloudapi.com" /etc/squid/squid.conf
+sed -i "1i\acl whitelist dstdomain -i cbs.tencentcloudapi.com" /etc/squid/squid.conf
+sed -i "1i\acl whitelist dstdomain -i redis.tencentcloudapi.com" /etc/squid/squid.conf
+sed -i "1i\acl whitelist dstdomain -i vpc.tencentcloudapi.com" /etc/squid/squid.conf
+sed -i "1i\acl whitelist dstdomain -i mariadb.tencentcloudapi.com" /etc/squid/squid.conf
+sed -i "1i\acl whitelist dstdomain -i cdb.tencentcloudapi.com" /etc/squid/squid.conf
+sed -i "1i\acl whitelist dstdomain -i clb.tencentcloudapi.com" /etc/squid/squid.conf
+sed -i "1i\acl whitelist dstdomain -i bmlb.tencentcloudapi.com" /etc/squid/squid.conf
+sed -i "1i\acl whitelist dstdomain -i bm.tencentcloudapi.com" /etc/squid/squid.conf
+sed -i "1i\acl whitelist dstdomain -i mongodb.tencentcloudapi.com" /etc/squid/squid.conf
+
+sed -i "1i\acl whitelist dstdomain –i github.com" /etc/squid/squid.conf
+sed -i "1i\acl wecubehosts src 10.128.194.128/26    #db subnet" /etc/squid/squid.conf
+sed -i "1i\acl wecubehosts src 10.128.194.0/25      #app subnet" /etc/squid/squid.conf
+sed -i "1i\# squid config for WeCube" /etc/squid/squid.conf
+
+squid -k reconfigure
 
 echo "Start squid success !"
