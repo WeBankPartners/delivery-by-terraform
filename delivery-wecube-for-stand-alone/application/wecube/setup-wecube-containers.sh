f8205cffbef2a5c65ff92f1fe1ab96a821912a7c
Tong Zhang <tz.tong.zhang@hotmail.com>
2020-03-12 16:21:52 +0800

    extract wecube container setup in separate sciprt file

---
+++ delivery-wecube-for-stand-alone/application/wecube/setup-wecube-containers.sh
@@ -0,0 +1,19 @@
+install_target_host=$1
+mysql_password=$2
+wecube_version=$3
+wecube_home=${4:-/data/wecube}
+
+mkdir -p $wecube_home/plugin
+
+sed "s~{{WECUBE_HOME}}~$wecube_home~g" wecube-db.tpl > wecube-db.yml
+sed "s~{{MYSQL_USER_PASSWORD}}~$mysql_password~g" wecube-db.tpl > wecube-db.yml
+echo "Starting wecube database ..."
+docker-compose -f wecube-db.yml up -d
+sleep 120
+
+echo "Starting wecube platform ..."
+sed -i "s~{{WECUBE_HOME}}~$wecube_home~g" wecube.cfg
+sed -i "s~{{SINGLE_HOST}}~$install_target_host~g" wecube.cfg
+sed -i "s~{{SINGLE_PASSWORD}}~$mysql_password~g" wecube.cfg
+./deploy_generate_compose.sh wecube.cfg ${wecube_version}
+docker-compose -f docker-compose.yml up -d
