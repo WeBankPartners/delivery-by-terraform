e6d7b27b75e1d8dc0f58ea223d976ab985d62cfb
Tong Zhang <tz.tong.zhang@hotmail.com>
2020-03-12 18:19:32 +0800

    add sql script to update host and port for deployment

--- delivery-wecube-for-stand-alone/application/wecube/setup-wecube-containers.sh
+++ delivery-wecube-for-stand-alone/application/wecube/setup-wecube-containers.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 install_target_host=$1
 mysql_password=$2
 wecube_version=$3
@@ -5,16 +7,17 @@ wecube_home=${4:-/data/wecube}
 
 mkdir -p $wecube_home/plugin
 
+CONFIG_FILE="wecube.cfg"
+sed -i "s~{{WECUBE_HOME}}~$wecube_home~g" $CONFIG_FILE
+sed -i "s~{{SINGLE_HOST}}~$install_target_host~g" $CONFIG_FILE
+sed -i "s~{{SINGLE_PASSWORD}}~$mysql_password~g" $CONFIG_FILE
+
 echo "Starting wecube database ..."
-cp wecube-db.tpl wecube-db.yml
-sed -i "s~{{WECUBE_HOME}}~$wecube_home~g" wecube-db.yml
-sed -i "s~{{MYSQL_USER_PASSWORD}}~$mysql_password~g" wecube-db.yml
+./generate_sql_file.sh $CONFIG_FILE
+./generate_db_compose_file.sh $CONFIG_FILE
 docker-compose -f wecube-db.yml up -d
 sleep 120
 
 echo "Starting wecube platform ..."
-sed -i "s~{{WECUBE_HOME}}~$wecube_home~g" wecube.cfg
-sed -i "s~{{SINGLE_HOST}}~$install_target_host~g" wecube.cfg
-sed -i "s~{{SINGLE_PASSWORD}}~$mysql_password~g" wecube.cfg
-./deploy_generate_compose.sh wecube.cfg ${wecube_version}
+./deploy_generate_compose.sh $CONFIG_FILE ${wecube_version}
 docker-compose -f docker-compose.yml up -d
