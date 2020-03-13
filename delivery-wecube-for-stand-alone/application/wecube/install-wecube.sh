a0e6df28c36aac6845a1153348e83ced71b273b3
Tong Zhang <tz.tong.zhang@hotmail.com>
2020-03-13 17:03:37 +0800

    adjust command order according to review comments

--- delivery-wecube-for-stand-alone/application/wecube/install-wecube.sh
+++ delivery-wecube-for-stand-alone/application/wecube/install-wecube.sh
@@ -7,9 +7,9 @@ echo "OPTIONS=-H unix:///var/run/docker.sock -H tcp://0.0.0.0:2375" >> /etc/sysc
 systemctl enable docker.service
 systemctl start docker.service
 
+./setup-wecube-containers.sh $@
+
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf 
 echo "net.bridge.bridge-nf-call-ip6tables = 1" >> /etc/sysctl.conf 
 echo "net.bridge.bridge-nf-call-iptables = 1" >> /etc/sysctl.conf 
 sysctl -p 
-
-./setup-wecube-containers.sh $@
