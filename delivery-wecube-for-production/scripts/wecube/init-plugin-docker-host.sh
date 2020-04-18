c813012cccb7bc1f64f6372851832075fa5a3784
huanghaixin@126.com <huanghaixin@126.com>
2020-04-19 03:20:27 +0800

    Support tencent cloud production version

--- delivery-wecube-for-production/scripts/wecube/init-plugin-docker-host.sh
+++ delivery-wecube-for-production/scripts/wecube/init-plugin-docker-host.sh
@@ -6,6 +6,7 @@ echo "remoteHost=$1,password=$2"
 echo `pwd`
 remoteHost=$1
 password=$2
+s3_port=$3
 
 sshpass -p ${password} ssh -o "StrictHostKeyChecking no" root@${remoteHost}  > /dev/null 2>&1 << remoteCmd
 
@@ -16,8 +17,23 @@ echo "OPTIONS='-H unix://var/run/docker.sock -H tcp://0.0.0.0:2375'" >> /etc/sys
 systemctl start docker.service
 systemctl enable docker.service
 
-echo "export http_proxy='http://10.128.194.2:3128'" >> /etc/profile
-echo "export https_proxy='http://10.128.194.2:3128'" >> /etc/profile
+mkdir -p /data/wecube/plugin
+mkdir -p /etc/systemd/system/docker.service.d
+cat > /etc/systemd/system/docker.service.d/https-proxy.conf << EOF
+[Service]
+Environment="HTTP_PROXY=http://10.128.199.3:3128" "HTTPS_PROXY=http://10.128.199.3:3128" "NO_PROXY=localhost,127.0.0.1,docker-registry.example.com,"
+EOF
+systemctl daemon-reload
+systemctl restart docker
+
+yum install docker-compose -y
+
+cd /root/
+sed "s~{{S3_PORT}}~$s3_port~g" wecube-s3.tpl > wecube-s3.yml
+docker-compose -f wecube-s3.yml up -d
+
+echo "export http_proxy='http://10.128.199.3:3128'" >> /etc/profile
+echo "export https_proxy='http://10.128.199.3:3128'" >> /etc/profile
 
 source /etc/profile
 
--- delivery-wecube-for-production/scripts/wecube/init-wecube-platform-host.sh
+++ delivery-wecube-for-production/scripts/wecube/init-wecube-platform-host.sh
@@ -8,21 +8,16 @@ echo `pwd`
 remoteHost=$1
 password=$2
 image_version=$3
+config_file=$4
+s3_port=$5
 
 sshpass -p ${password} ssh -o "StrictHostKeyChecking no" root@${remoteHost}  > /dev/null 2>&1 << remoteCmd
-
 
 chmod +x /root/wecube-platform-scripts/*
 yum install -y dos2unix
 dos2unix /root/wecube-platform-scripts/*
 cd /root/wecube-platform-scripts/
-./install-wecube-platform.sh wecube-platform.cfg ${image_version} > install-wecube.log 2>&1
-
-
-echo "export http_proxy='http://10.128.194.2:3128'" >> /etc/profile
-echo "export https_proxy='http://10.128.194.2:3128'" >> /etc/profile
-
-source /etc/profile
+./install-wecube-platform.sh $config_file ${image_version} $s3_port > install-wecube.log 2>&1
 
 exit
 remoteCmd
