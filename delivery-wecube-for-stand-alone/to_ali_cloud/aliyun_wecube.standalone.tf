2b5a0d8edff04e35e0fe5b670b93244b2d810716
huanghaixin@126.com <huanghaixin@126.com>
2020-05-23 10:27:41 +0800

    Modify the encrypted password

--- delivery-wecube-for-stand-alone/to_ali_cloud/aliyun_wecube.standalone.tf
+++ delivery-wecube-for-stand-alone/to_ali_cloud/aliyun_wecube.standalone.tf
@@ -1,10 +1,10 @@
 #全局变量
 variable "instance_root_password" {
-  # default = "Wecube@123456"
+  default = "Wecube@123456"
 }
 
 variable "mysql_root_password" {
-  # default = "Wecube@123456"
+  default = "Wecube@123456"
 }
 
 variable "wecube_version" {
@@ -124,8 +124,19 @@ resource "alicloud_instance" "instance_wecube_platform" {
   }
 
   provisioner "file" {
-    source      = "../application/"
+    source      = "../application-for-alicloud/"
     destination = "${var.wecube_home}/installer"
+  }
+
+  provisioner "file" {
+    content = templatefile("${path.module}/../application-for-alicloud/wecube/database/cmdb/01.register_cmdb_asset_ids.sql.tpl", {
+        wecube_vpc_asset_id = alicloud_vpc.vpc.id
+        wecube_subnet_asset_id = alicloud_vswitch.switch_app.id
+        wecube_route_table_asset_id = alicloud_vpc.vpc.route_table_id
+        wecube_host_asset_id = alicloud_instance.instance_wecube_platform.id
+      }
+    )
+    destination = "${var.wecube_home}/installer/wecube/database/cmdb/01.register_cmdb_asset_ids.sql"
   }
 
   provisioner "remote-exec" {
