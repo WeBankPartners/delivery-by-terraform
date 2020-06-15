1e18fbd98971accf1b6ea426b0868a92bf392fb5
Tong Zhang <tz.tong.zhang@hotmail.com>
2020-06-15 11:29:18 +0800

    merge main.tf into variables.tf for modules

--- module/planning/variables.tf
+++ module/planning/variables.tf
@@ -3,3 +3,10 @@ variable "idc_prefix" {}
 variable "wecube_release_version" {}
 variable "initial_password" {}
 variable "default_mysql_port" {}
+
+
+locals {
+  cluster_mode                = length(var.availability_zones) > 1
+  primary_availability_zone   = var.availability_zones[0]
+  secondary_availability_zone = local.cluster_mode ? var.availability_zones[0] : null
+}
