edc832ae50a1f922aec7d4c1fe21fc2042abb9a0
Tong Zhang <tz.tong.zhang@hotmail.com>
2020-06-24 15:16:00 +0800

    bring back logic to create database

--- installer/db-connectivity/install-db-connectivity.sh
+++ installer/db-connectivity/install-db-connectivity.sh
@@ -6,7 +6,12 @@ ENV_FILE=$1
 
 source $ENV_FILE
 
-echo "Verifying connectivity to database $DB_NAME at $DB_HOST:$DB_PORT ..."
+echo "Creating database $DB_NAME at $DB_HOST:$DB_PORT ..."
+../execute-sql-expression.sh $DB_HOST $DB_PORT \
+  mysql $DB_USERNAME $DB_PASSWORD \
+  "CREATE DATABASE IF NOT EXISTS \`$DB_NAME\`;"
+
+echo "Verifying connectivity to database $DB_NAME ..."
 ../wait-for-it.sh -t 60 "$DB_HOST:$DB_PORT"
 ../execute-sql-expression.sh $DB_HOST $DB_PORT \
   $DB_NAME $DB_USERNAME $DB_PASSWORD \
