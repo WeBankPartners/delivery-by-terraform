3ad75e31e61da2b7ba117462a988341a46a0ccda
Tong Zhang <tz.tong.zhang@hotmail.com>
2020-09-29 17:06:12 +0800

    substitue docker compose env file instead of copying from bash env directly

--- installer/mysql-docker/install-mysql-docker.sh
+++ installer/mysql-docker/install-mysql-docker.sh
@@ -7,9 +7,10 @@ ENV_FILE=$1
 source $ENV_FILE
 
 echo "Installing mysql docker container on $HOST_PRIVATE_IP"
-
-cp $ENV_FILE ./.env
-docker-compose -f "./wecube-db.yml" up -d
+DOCKER_COMPOSE_ENV_TEMPLATE_FILE="./wecube-db.env.template"
+DOCKER_COMPOSE_ENV_FILE="./wecube-db.env"
+../substitute-in-file.sh $ENV_FILE $DOCKER_COMPOSE_ENV_TEMPLATE_FILE $DOCKER_COMPOSE_ENV_FILE
+docker-compose -f "./wecube-db.yml" --env-file=$DOCKER_COMPOSE_ENV_FILE up -d
 ../wait-for-it.sh -t 120 "$HOST_PRIVATE_IP:$MYSQL_PORT" -- echo "Mysql docker container is ready."
 
 echo "Installation of mysql docker container completed."
