fe80ea3508660bea36505fa734f4db6464aebfd1
Tong Zhang <tz.tong.zhang@hotmail.com>
2020-09-29 17:19:14 +0800

    substitue docker compose env file instead of copying from bash env directly for minio-docker

--- installer/minio-docker/install-minio-docker.sh
+++ installer/minio-docker/install-minio-docker.sh
@@ -8,8 +8,10 @@ source $ENV_FILE
 
 echo "Installing minio docker container on $HOST_PRIVATE_IP"
 
-cp $ENV_FILE ./.env
-docker-compose -f "./wecube-s3.yml" up -d
+DOCKER_COMPOSE_ENV_TEMPLATE_FILE="./wecube-s3.env.template"
+DOCKER_COMPOSE_ENV_FILE="./wecube-s3.env"
+../substitute-in-file.sh $ENV_FILE $DOCKER_COMPOSE_ENV_TEMPLATE_FILE $DOCKER_COMPOSE_ENV_FILE
+docker-compose -f "./wecube-s3.yml" --env-file=$DOCKER_COMPOSE_ENV_FILE up -d
 ../wait-for-it.sh -t 60 "$HOST_PRIVATE_IP:$S3_PORT" -- echo "Minio docker container is ready."
 
 echo "Installation of minio docker container completed."
