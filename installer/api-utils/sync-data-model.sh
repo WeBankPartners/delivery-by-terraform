8a9941e59aee9fac7dfcbca910dd0ca8fa8b5718
Tong Zhang <tz.tong.zhang@hotmail.com>
2021-01-27 12:59:29 +0800

    check if POST method is supported before sync data model

--- installer/api-utils/sync-data-model.sh
+++ installer/api-utils/sync-data-model.sh
@@ -12,16 +12,26 @@ SCRIPT_DIR=$(dirname "$0")
 
 [ -z "${ACCESS_TOKEN}" ] && ACCESS_TOKEN=$(${SCRIPT_DIR}/login.sh ${SYS_SETTINGS_ENV_FILE})
 
-DATA_MODEL=$(curl -sSfL \
-	--request GET "http://${CORE_HOST}:19090/platform/v1/models/package/${PLUGIN_PACKAGE_NAME}" \
+ALLOWED_METHODS=$(curl -sSfLi \
+	--request OPTIONS "http://${CORE_HOST}:19090/platform/v1/models/package/${PLUGIN_PACKAGE_NAME}" \
 	--header "Authorization: Bearer ${ACCESS_TOKEN}" \
-	| ${SCRIPT_DIR}/check-status-in-json.sh \
-	| jq --exit-status '.data'
+	| grep 'Allow:'
 )
 
-curl -sSfL --request POST "http://${CORE_HOST}:19090/platform/v1/models" \
+RESPONSE_JSON=$(curl -sSfL \
+	--request GET "http://${CORE_HOST}:19090/platform/v1/models/package/${PLUGIN_PACKAGE_NAME}" \
 	--header "Authorization: Bearer ${ACCESS_TOKEN}" \
-	--header 'Content-Type: application/json' \
-	--data @- <<<"${DATA_MODEL}" \
-	| ${SCRIPT_DIR}/check-status-in-json.sh \
-	| jq --exit-status '{status: .status, message: .message, data: {id: .data.id, version: .data.version, packageName: .data.packageName}}'
+	| ${SCRIPT_DIR}/check-status-in-json.sh
+)
+DATA_MODEL=$(jq '.data' <<<"$RESPONSE_JSON")
+
+if [ "${ALLOWED_METHODS/POST/}" != "${ALLOWED_METHODS}" ]; then
+	curl -sSfL --request POST "http://${CORE_HOST}:19090/platform/v1/models" \
+		--header "Authorization: Bearer ${ACCESS_TOKEN}" \
+		--header 'Content-Type: application/json' \
+		--data @- <<<"${DATA_MODEL}" \
+		| ${SCRIPT_DIR}/check-status-in-json.sh \
+		| jq --exit-status '{status: .status, message: .message, data: {id: .data.id, version: .data.version, packageName: .data.packageName}}'
+else
+	jq '{status: .status, message: .message, data: {id: .data.id, version: .data.version, packageName: .data.packageName}}' <<<"$RESPONSE_JSON"
+fi
