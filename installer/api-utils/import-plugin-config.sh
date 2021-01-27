6299eb871e581cd65b666ecd87c1eeed14f5eb26
Tong Zhang <tz.tong.zhang@hotmail.com>
2021-01-27 15:18:23 +0800

    rename plugin package coords to plugin package id

--- installer/api-utils/import-plugin-config.sh
+++ installer/api-utils/import-plugin-config.sh
@@ -4,7 +4,7 @@ set -e
 set -o pipefail
 
 SYS_SETTINGS_ENV_FILE=$1
-PLUGIN_PKG_COORDS=$2
+PLUGIN_PKG_ID=$2
 PLUGIN_CONFIG_FILE=$3
 
 source $SYS_SETTINGS_ENV_FILE
@@ -14,7 +14,7 @@ SCRIPT_DIR=$(dirname "$0")
 [ -z "${ACCESS_TOKEN}" ] && ACCESS_TOKEN=$(${SCRIPT_DIR}/login.sh ${SYS_SETTINGS_ENV_FILE})
 
 curl -sSfL \
-	--request POST "http://${CORE_HOST}:19090/platform/v1/plugins/packages/import/$PLUGIN_PKG_COORDS" \
+	--request POST "http://${CORE_HOST}:19090/platform/v1/plugins/packages/import/$PLUGIN_PKG_ID" \
 	--header "Authorization: Bearer ${ACCESS_TOKEN}" \
 	--form xml-file=@"${PLUGIN_CONFIG_FILE}" \
 	| ${SCRIPT_DIR}/check-status-in-json.sh
