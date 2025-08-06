1f7731eff2eb0a02e32652192d2a86a7a715b879
zgyzgyhero <894526647@qq.com>
2025-08-06 09:43:23 +0800

    update init bash

--- installer/wecube-system-settings/settings/init.sh
+++ installer/wecube-system-settings/settings/init.sh
@@ -1,4 +1,4 @@
-PLUGIN_NAMES='wecube-plugins-wecmdb wecube-plugins-saltstack wecube-plugins-monitor wecube-plugins-terraform wecube-plugins-artifacts wecube-plugins-taskman'
+PLUGIN_NAMES='wecube-plugins-wecmdb wecube-plugins-saltstack wecube-plugins-monitor wecube-plugins-terraform wecube-plugins-artifacts wecube-plugins-taskman wecube-plugins-itsdangerous wecube-plugins-terminal'
 
 PLUGIN_CONFIG=''
 
